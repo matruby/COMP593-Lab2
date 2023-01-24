2e88d76afb9785716cae0b5ebcac1cd01c0706ce
matruby <Matthew.Rubie@flemingcollege.ca>
2023-01-24 16:15:52 -0500

    Lab2_Main.py
    - Added a function view_name_id( )
    - Cleaned up Comments

--- Lab2_Main.py
+++ Lab2_Main.py
@@ -1,8 +1,7 @@
 
-# Step 1 Start Writing the Script
 def main():
-    """Personal Profile"""
-    # Step 2 Create a Complex Data Structure
+    """Some facts about me in dictionary"""
+    # Create the dictionary of my personal information
     personal_dict = {
             'full name': 'Matthew Rubie',
             'student_id': 10197289,
@@ -12,12 +11,28 @@ def main():
                 {'title':'superbad', 'genre':'comedy'},
                 ]
             }
-    # Step 3 Add Another Movie to the Data Structure 
+
+    # Add a new movie to the dictionary
     personal_dict['movies'].append({'title':'us', 'genre':'horror'})
+
+    name_id_str = view_name_id(personal_dict)
     return 
 
+def view_name_id(comp_ds):
+    """Prints out name and student ID"""
+    # Get name and student ID from dictionary
+    full_name = comp_ds['full name'] 
+    student_id = comp_ds['student_id']
+
+    # Use split to get the first name
+    first_name = full_name.split()[0]
+
+    # Strings to be returned
+    name_str = f'My name is {full_name}, but you can call me Sir {first_name}\n' + \
+    f'My student ID is {student_id}'
+
+    return name_str 
 
 if __name__ == '__main__': 
     main()
-    
 
