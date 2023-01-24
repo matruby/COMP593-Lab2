adf7aee1d2a92156bbb0309ad1357dd253178cc2
matruby <Matthew.Rubie@flemingcollege.ca>
2023-01-24 14:46:53 -0500

    Initial Commit
    Main Program File + readme file

---
+++ Lab2_Main.py
@@ -0,0 +1,19 @@
+
+# Step 1 Start Writing the Script
+def main():
+    """Personal Profile"""
+    # Step 2 Create a Complex Data Structure
+    personal_dict = {
+            'full name': 'Matthew Rubie',
+            'student_id': 10197289,
+            'pizza_toppings': ['PEPPERONI', 'MUSHROOMS', 'HAM'],
+            'movies': [
+                {'title':'the dark knight', 'genre':'action/superhero'},
+                {'title':'superbad', 'genre':'comedy'},
+                ]
+            }
+    # Step 3 Add Another Movie to the Data Structure 
+    return
+if __name__ == '__main__': 
+    main()
+
